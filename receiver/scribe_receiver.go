59d46d5e3415dc2b5fe210d4bfa26a064852d3ab
Peter Csizsek <peter.csizsek@gmail.com>
2014-03-27 20:35:28 +0100

    siplified scribe parsing

--- receiver/scribe_receiver.go
+++ receiver/scribe_receiver.go
@@ -6,11 +6,9 @@ import "net/rpc"
 import "github.com/csizsek/prism/entity"
 import "github.com/prezi/go-thrift/examples/scribe"
 import "github.com/prezi/go-thrift/thrift"
-import "github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
 
 type ScribeReceiver struct {
 	output     chan entity.ScribeEntity
-	lineRegExp pcre.Regexp
 	port       int
 	listener   net.Listener
 }
@@ -25,10 +23,8 @@ func (this *ScribeReceiver) Receive() {
 func NewScribeReceiver(output chan entity.ScribeEntity) *ScribeReceiver {
 	receiver := new(ScribeReceiver)
 	receiver.output = output
-	receiver.lineRegExp = pcre.MustCompile(`^(?P<TimeStamp>\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d{3}) (\S+) (?P<Category>\S+) (?P<Message>.+)$`, 0)
 	rpc.RegisterName("Thrift", &scribe.ScribeServer{receiver})
 	receiver.port = 1234
-	rpc.RegisterName("Thrift", &scribe.ScribeServer{receiver})
 	receiver.listener, _ = net.Listen("tcp", "127.0.0.1:1234")
 	return receiver
 }
@@ -43,12 +39,8 @@ func (this *ScribeReceiver) Log(messages []*scribe.LogEntry) (scribe.ResultCode,
 
 func (this *ScribeReceiver) parseMessage(msg *scribe.LogEntry) *entity.ScribeEntity {
 	scribeEntity := entity.NewScribeEntity()
-	if m := this.lineRegExp.MatcherString(msg.Message, 0); m != nil {
-		scribeEntity.Category = m.GroupString(3)
-		scribeEntity.Message = m.GroupString(4)
-	} else {
-		fmt.Println("no match :(")
-		fmt.Println(msg.Message)
-	}
+	scribeEntity.Category = msg.Category
+	scribeEntity.Message = msg.Message
+	fmt.Println(scribeEntity.Category, scribeEntity.Message)
 	return scribeEntity
 }
